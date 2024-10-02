ad93a5d3417e544309cc9e6352e5918f1ff98405
sm48-ai <javacom894@gmail.com>
2024-10-02 09:37:27 +0530

    ne changes

--- main.java
+++ main.java
@@ -1,9 +1,9 @@
 import java.util.Date;
-public class Main{
-    public static void main(String args[]){
-        System.out.println("This is java  program");
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hi this is main method");
         System.out.println(new Date());
-        System.out.println("Hello");
-        System.out.println("Added one more fole");
     }
+    
 }
