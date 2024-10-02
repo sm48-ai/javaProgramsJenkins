7612f216df0b92831ceba651d0d0028744a731b3
sm48-ai <javacom894@gmail.com>
2024-10-02 09:14:29 +0530

    Added one more file

--- main.java
+++ main.java
@@ -4,5 +4,6 @@ public class Main{
         System.out.println("This is java  program");
         System.out.println(new Date());
         System.out.println("Hello");
+        System.out.println("Added one more fole");
     }
 }
