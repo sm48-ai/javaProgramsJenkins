cae5d68c6af68986c0180ff7726c80833e475559
sm48-ai <javacom894@gmail.com>
2024-10-02 09:19:31 +0530

    Added one more file

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 import java.util.Date;
-public class Main{
+public class main{
     public static void main(String args[]){
         System.out.println("This is java  program");
         System.out.println(new Date());
