9a7bba1394f87776c273e0af4cec8af6bd2a4762
sm48-ai <javacom894@gmail.com>
2024-10-02 09:58:53 +0530

    Student Object Created

---
+++ Student.java
@@ -0,0 +1,10 @@
+
+
+public class Student {
+    public Student(){
+        System.out.println("Creating Student Object");
+    }
+    public void sayHello(){
+        System.out.println("Hi i am student");
+    }
+}
