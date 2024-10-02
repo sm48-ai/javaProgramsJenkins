8c8704aefe947b80d5218b21574f7fefed8560c0
sm48-ai <javacom894@gmail.com>
2024-10-02 10:54:06 +0530

    satish

--- main.java
+++ main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi this is main method");
         System.out.println(new Date());
+        System.out.println("Hello i am satish prajapati");
         Student student=new Student();
         student.sayHello();
     }
