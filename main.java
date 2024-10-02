1b253006130ec8476320d8ffe78b3cb4dabb2e10
sm48-ai <javacom894@gmail.com>
2024-10-02 08:37:16 +0530

    initial commit

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ public class Main{
     public static void main(String args[]){
         System.out.println("This is java  program");
         System.out.println(new Date());
+        System.out.println("Hello");
     }
 }
