ca417eea5027dca9751122c49d9352e9247155c0
sm48-ai <javacom894@gmail.com>
2024-10-03 10:57:18 +0530

    first

--- Student.java
+++ Student.java
@@ -6,6 +6,6 @@ public class Student {
     }
     public void sayHello(){
         System.out.println("Hi i am student");
-        System.out.println("Hi adding second line of code");
+        System.out.println("Hi adding second line o");
     }
 }
